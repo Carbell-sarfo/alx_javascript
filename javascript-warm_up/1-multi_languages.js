3a023d544ac866b59f8f5f95ac1659ec49f9ab32
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 12:44:41 +0000

    languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,12 @@
-// First Contant, First Print
-// This script looks to print a variable in the console.
+// 1.3 languages
+// This script looks to print a three lines in the console.
+
+
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'Javascript is amazing';
+
+// printing the various lines in the console
+console.log(line1);
+console.log(line2);
+console.log(line3);
