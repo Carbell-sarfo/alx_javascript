420ee9438139c403816410a281f4fb0b2de53e47
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 13:24:43 +0000

    objects

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,22 @@
+// 8. Increment object
+// Update the script by adding a new function incr that increments the integer value
+
+
+const myObject = {
+    type: 'object',
+    value: 12,
+    incr: function () {
+      this.value++;
+    }
+  };
+  
+  console.log(myObject);
+  
+  myObject.incr();
+  console.log(myObject);
+  
+  myObject.incr();
+  console.log(myObject);
+  
+  myObject.incr();
+  console.log(myObject);
