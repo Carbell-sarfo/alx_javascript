8ca039cd74cbd38edd19b5aadcc0dccf644bbf9c
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 15:45:17 +0000

    trying 3 again

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -7,8 +7,9 @@ if (process.argv.length !== 3) {
 }
 
 const apiUrl = process.argv[2];
+const characterId = 18; // ID for Wedge Antilles
 
-// Send a GET request to the Star Wars API to retrieve all films
+// Send a GET request to the specified API URL
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error.message);
@@ -19,20 +20,22 @@ request.get(apiUrl, (error, response, body) => {
   if (response && response.statusCode === 200) {
     try {
       const filmsData = JSON.parse(body);
-      const characterId = '18'; // Character ID for "Wedge Antilles"
-      
-      // Filter films where "Wedge Antilles" is present
-      const moviesWithWedge = filmsData.results.filter((film) => {
-        return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
-      });
 
-      console.log(moviesWithWedge.length);
+      // Filter films to find how many times Wedge Antilles appears
+      const numberOfFilms = filmsData.results.reduce((count, film) => {
+        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+          return count + 1;
+        }
+        return count;
+      }, 0);
+
+      console.log(numberOfFilms);
     } catch (parseError) {
       console.error('Failed to parse JSON response.');
       process.exit(1);
     }
   } else {
-    console.error(`Failed to retrieve movie data. Status code: ${response.statusCode}`);
+    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
     process.exit(1);
   }
 });
