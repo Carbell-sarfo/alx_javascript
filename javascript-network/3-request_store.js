228aa7d4011fd93f70a0703c70692c33bb6e6e23
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 15:40:42 +0000

    tring out 3 again

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 // Check if the URL and file path are provided as command line arguments
 if (process.argv.length !== 4) {
-  console.error('Usage: node 3-request_store.js <URL> <File Path>');
+  console.error('Usage: node webpage_to_file.js <URL> <File Path>');
   process.exit(1);
 }
 
@@ -17,7 +17,7 @@ request.get(url, (error, response, body) => {
     process.exit(1);
   }
 
-  // Check if the response contains a valid status code
+  // Check if the response contains a valid status code (usually 200 for success)
   if (response && response.statusCode === 200) {
     // Save the response body to the specified file (UTF-8 encoded)
     fs.writeFile(filePath, body, 'utf-8', (writeError) => {
@@ -25,7 +25,7 @@ request.get(url, (error, response, body) => {
         console.error('Failed to write to the file:', writeError);
         process.exit(1);
       }
-      console.log('Response body written to', filePath);
+      console.log(`Response body from ${url} saved to ${filePath}`);
     });
   } else {
     console.error(`Failed to retrieve web page. Status code: ${response.statusCode}`);
