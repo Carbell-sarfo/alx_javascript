0ac54ddf1c82f32d5349c5df3b8442392076fba9
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 12:29:10 +0000

    Javacript is amazing

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+// First Contant, First Print
+// This script looks to print a variable in the console.
+
+
+// decalare my variable and then log my variable in the console.
+const myVar = 'JavaScript is amazing';
+
+console.log (myVar);
---
+++ javascript-warm_up/README.md
@@ -0,0 +1,4 @@
+# Javascript is amazing
+
+This is the Warm Up Javascript file.
+We are looking at Basic Javascript and getting things 
