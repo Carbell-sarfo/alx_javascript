a6db2328d94375f028f80508db2388cc6633bd4c
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 15:06:01 +0000

    6-square

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// 6-square.js
+
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+  constructor(size) {
+    super(size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
