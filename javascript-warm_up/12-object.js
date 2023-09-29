c326607f1944bebb1aa512aca860304a5c917d34
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 13:02:32 +0000

    updating an array

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+// 3. Object
+//  Script is to update the value 12 with 89
+
+// #!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.value = 89;
+
+console.log(myObject);
